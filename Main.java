fcea32fdb031f53ff178708246a270bfb6c5116f
Shenum1 <131256802+Shenum1@users.noreply.github.com>
2024-05-06 03:24:18 -0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main{
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("What's your favorite food? ");
+        String food = scanner.nextLine();
+
+        System.out.println("What's your name? ");
+        String name = scanner.nextLine();
+
+        System.out.println("How old are you : ");
+        int age = scanner.nextInt();
+
+        System.out.println("Your favorite food is "+food);
+        System.out.println("Hello "+name);
+        System.out.println("You are "+age);
+
+
+
+
+    }
+
+
+
+}
